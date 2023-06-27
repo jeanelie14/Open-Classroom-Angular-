6a79642048067d9dc35902cadc8910ff49c1077e
jeanelie14 <jeune14@gmail.com>
2023-06-27 00:56:45 -0400

    avant dernière section

--- snapFace/src/app/components/nav/nav.component.ts
+++ snapFace/src/app/components/nav/nav.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
-export class NavComponent {
-
-}
+export class NavComponent {}
